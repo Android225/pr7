06b7db27efc6e2c266f523259ad7622240a9747b
Andry <a.krishtofor@gmail.com>
2023-04-21 22:05:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pr7/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.pr7;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+public class MainActivity extends AppCompatActivity {
+
+    sec_frag second__ = new sec_frag();
+
+    thi_frag third__ = new thi_frag();
+
+    fir_frag first__ = new fir_frag();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment);
+
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.fragment_container_view,first__)
+                .commit();
+    }
+
+    public void second_click(View view) {
+
+        FragmentTransaction fTrans = getSupportFragmentManager().beginTransaction();
+
+        Bundle bundle = new Bundle();
+
+        second__.setArguments(bundle);
+
+        fTrans.replace(R.id.fragment_container_view, second__).addToBackStack(null).commit();
+
+    }
+
+    public void third_click(View view) {
+        FragmentTransaction fTrans = getSupportFragmentManager().beginTransaction();
+
+        Bundle bundle = new Bundle();
+
+        third__.setArguments(bundle);
+
+        fTrans.replace(R.id.fragment_container_view, third__).addToBackStack(null).commit();
+
+    }
+}
---
+++ app/src/main/java/com/example/pr7/fir_frag.java
@@ -0,0 +1,9 @@
+package com.example.pr7;
+
+import androidx.fragment.app.Fragment;
+
+public class fir_frag extends Fragment{
+    public fir_frag(){
+        super(R.layout.first_fragment);
+    }
+}
